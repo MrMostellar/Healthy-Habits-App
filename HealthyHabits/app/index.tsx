ce6f9155e8bacfff58bf3a774234d703e7cc9673
MrMostellar <mr.mostellar@gmail.com>
2024-10-04 22:59:16 -0600

    Added sign in functions

--- HealthyHabits/app/index.tsx
+++ HealthyHabits/app/index.tsx
@@ -1,29 +1,22 @@
+import React from 'react';
 import { StyleSheet, Text, View, ScrollView, ScrollViewComponent } from 'react-native';
 import { ExpoRouter } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Post from '../components/Post';
+import Recipe from '../components/Recipe';
+import SignIn from '../tabs/SignIn';
+import HomePage from '../tabs/HomePage';
 
 export default function App() {
+  {/*create a state for isSignedIn*/}
+  const [isSignedIn, setIsSignedIn] = React.useState(false);
   return (
     <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            
+            {isSignedIn ?
+              <HomePage/>:
+              <SignIn/>
+            }
+
         </ScrollView>
     </SafeAreaView>
   );
---
+++ HealthyHabits/tabs/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
+
+export default function HomePage(){
+    return(
+        <View>
+            <Text>Home Page</Text>
+        </View>
+    )
+}
